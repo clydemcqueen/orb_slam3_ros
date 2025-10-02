1d939db2108594c9953e29133e565f29f267d59c
Clyde McQueen <clyde@mcqueen.net>
2025-10-02 15:35:03 -0700

    Compiles on 24.04

--- src/mono_imu.cpp
+++ src/mono_imu.cpp
@@ -1,4 +1,4 @@
-#include "cv_bridge/cv_bridge.h"
+#include "cv_bridge/cv_bridge.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
