7c647df74d3993e3d130efde0e6d86bd4b65b48f
Clyde McQueen <clyde@mcqueen.net>
2025-10-23 11:19:04 -0700

    Add SlamStatus msg

--- orb_slam3_ros/src/mono.cpp
+++ orb_slam3_ros/src/mono.cpp
@@ -1,5 +1,6 @@
 #include "cv_bridge/cv_bridge.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "orb_slam3_msgs/msg/slam_status.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
@@ -19,6 +20,7 @@ class MonoNode final : public rclcpp::Node
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
 
     // Declare publications
+    rclcpp::Publisher<orb_slam3_msgs::msg::SlamStatus>::SharedPtr status_pub_;
     rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr cam_pub_;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr map_pub_;
 
@@ -29,11 +31,9 @@ class MonoNode final : public rclcpp::Node
         "install/orb_slam3_bringup/share/orb_slam3_bringup/param/euroc_mav.yaml");
     std::string world_frame_id_ = declare_parameter<std::string>("world_frame_id",
         "world");
-    bool show_viewer_ = declare_parameter<bool>("show_viewer",
-        false);
 
     // Start ORB_SLAM3
-    ORB_SLAM3::System slam_{voc_file_, settings_file_, ORB_SLAM3::System::MONOCULAR, show_viewer_};
+    ORB_SLAM3::System slam_{voc_file_, settings_file_, ORB_SLAM3::System::MONOCULAR, false};
 
     // Keep track of the SLAM state
     int tracking_state_{};
@@ -44,6 +44,7 @@ public:
     MonoNode() :
         Node("orb_slam3_mono_node")
     {
+        status_pub_ = create_publisher<orb_slam3_msgs::msg::SlamStatus>("slam_status", 10);
         cam_pub_ = create_publisher<geometry_msgs::msg::PoseStamped>("camera_pose", 10);
         map_pub_ = create_publisher<sensor_msgs::msg::PointCloud2>("map_points", 10);
 
@@ -91,9 +92,30 @@ public:
 
         if (tracking_state == ORB_SLAM3::Tracking::eTrackingState::OK) {
             // Publish results
-            publish_pose(image_msg->header.stamp, t_cam_world);
+            publish_pose(image_msg->header.stamp, t_cam_world);  // TODO always publish?
             publish_map(image_msg->header.stamp);
         }
+
+        // TODO refactor
+
+        orb_slam3_msgs::msg::SlamStatus status_msg;
+        status_msg.header.stamp = image_msg->header.stamp;
+        status_msg.header.frame_id = world_frame_id_;
+
+        status_msg.tracking_state = static_cast<int8_t>(tracking_state);
+        status_msg.map_id = map_id;
+        status_msg.map_changed = map_changed;
+
+        // Invert Tcw to get the pose of the camera in the world frame
+        const auto t_world_cam = t_cam_world.inverse();
+        status_msg.pose = to_msg(t_world_cam);
+
+        // Be sure to send tracked points, smaller
+        const auto tracked_points = slam_.GetTrackedMapPoints();
+        const auto msg = to_msg(image_msg->header.stamp, world_frame_id_, tracked_points);
+        status_msg.tracked_points = msg;
+
+        status_pub_->publish(status_msg);
     }
 
     void publish_pose(const builtin_interfaces::msg::Time stamp, const Sophus::SE3f& t_cam_world) const
